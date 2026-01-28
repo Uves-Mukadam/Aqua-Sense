e6d2e4cbc3f5ba43b319895aca969cee517f5a03
sahilkazi1010 <kazisahil2006@gmail.com>
2026-01-28 12:39:33 +0530

    data folder

---
+++ data/city_data.py
@@ -0,0 +1,10 @@
+
+# City configuration for Aqua Sense
+CITY_DATA = {
+    "Andheri": {
+        "latitude": 19.1136,
+        "longitude": 72.8697,
+        "population": 1500000,
+        "avg_lpcd": 135 # Liters per capita per da
+    }
+}
